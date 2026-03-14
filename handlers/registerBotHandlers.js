8d5fc4034f1dfda4e5a24d2085c2869a8114c64f
alenapie <155849730+alenapie@users.noreply.github.com>
2026-03-14 12:12:52 +0300

    Add dedicated handlers file for future custom functions

--- handlers/registerBotHandlers.js
+++ handlers/registerBotHandlers.js
@@ -1,9 +1,11 @@
 const { registerQuizHandler } = require("./registerQuizHandler");
 const { registerYodomaHandler } = require("./registerYodomaHandler");
+const { registerCustomHandlers } = require("./registerCustomHandlers");
 
 function registerBotHandlers(bot) {
   registerQuizHandler(bot);
   registerYodomaHandler(bot);
+  registerCustomHandlers(bot);
 }
 
 module.exports = {
---
+++ handlers/registerCustomHandlers.js
@@ -0,0 +1,11 @@
+function registerCustomHandlers(bot) {
+  // Добавляй сюда новые обработчики по мере развития проекта.
+  // Пример:
+  // bot.command("help", async (ctx) => {
+  //   await ctx.reply("Справка по командам");
+  // });
+}
+
+module.exports = {
+  registerCustomHandlers,
+};
