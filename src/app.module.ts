78fb9311d95f076e45e2eb336ad6a2ae102e34a7
alenapie <alenapie@yandex.ru>
2026-04-06 11:12:31 +0300

    nest add fix package 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from "@nestjs/common";
-import { HttpModule } from "@nestjs/axios";
 import { BotModule } from "./bot/bot.module";
-import { DatabaseModule } from "./database/database.module";
 
 @Module({
-  imports: [HttpModule, DatabaseModule, BotModule],
+  imports: [BotModule],
 })
 export class AppModule {}
--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -1,12 +1,29 @@
 import { Module } from "@nestjs/common";
-import { HttpModule } from "@nestjs/axios";
 import { BotService } from "./bot.service";
-import { BotController } from "./bot.controller";
-import { DatabaseModule } from "../database/database.module";
+import { Pool } from "pg";
 
 @Module({
-  imports: [HttpModule, DatabaseModule],
-  providers: [BotService],
-  controllers: [BotController],
+  providers: [
+    BotService,
+    {
+      provide: "PG_POOL",
+      useFactory: async () => {
+        const pool = new Pool({
+          connectionString: process.env.DATABASE_URL,
+        });
+        // Создаём таблицу participants при старте
+        await pool.query(`
+          CREATE TABLE IF NOT EXISTS participants (
+            user_id BIGINT PRIMARY KEY,
+            username TEXT,
+            first_name TEXT,
+            last_name TEXT
+          );
+        `);
+        return pool;
+      },
+    },
+  ],
+  exports: [BotService],
 })
 export class BotModule {}
